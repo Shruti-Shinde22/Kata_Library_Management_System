5da73b54522edda0c97a59db7793263f85be8d8e
Shruti2208 <shindeshru2208@gmail.com>
2024-09-26 17:17:07 +0530

    Implemented BookInterface and Partial Book class

--- src/Main/Book.java
+++ src/Main/Book.java
@@ -1,5 +1,39 @@
 package Main;
 
-public class Book{
+public class Book implements BookInterface{
 
+    @Override
+    public String getIsbn() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public int getYear() {
+        return 0;
+    }
+
+    @Override
+    public void bookBorrow() throws Exception {
+
+    }
+
+    @Override
+    public void bookReturn() throws Exception {
+
+    }
+
+    @Override
+    public boolean isBorrowed() {
+        return false;
+    }
 }
