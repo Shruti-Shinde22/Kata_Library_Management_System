18ced0b3b9e543cead9a7192d3945ff9c4533fd7
Shruti2208 <shindeshru2208@gmail.com>
2024-09-27 01:22:36 +0530

    Add unit tests for Book class

--- src/Main/Book.java
+++ src/Main/Book.java
@@ -1,39 +1,59 @@
 package Main;
 
-public class Book implements BookInterface{
+public class Book implements BookInterface {
+    private final String isbn;
+    private final String title;
+    private final String author;
+    private final int year;
+    private boolean borrowed;
+
+    public Book(String isbn, String title, String author, int year) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.borrowed = false;
+    }
+
 
     @Override
     public String getIsbn() {
-        return null;
+        return isbn;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 
     @Override
     public String getAuthor() {
-        return null;
+        return author;
     }
 
     @Override
     public int getYear() {
-        return 0;
+        return year;
     }
 
     @Override
     public void bookBorrow() throws Exception {
-
+        if(borrowed){
+            throw new Exception("Book already Borrowed");
+        }
+        borrowed=true;
     }
 
     @Override
-    public void bookReturn() throws Exception {
-
+    public void bookReturn() throws Exception{
+        if(!borrowed){
+            throw new Exception("Book not Borrowed");
+        }
+        borrowed =false;
     }
 
     @Override
     public boolean isBorrowed() {
-        return false;
+        return borrowed;
     }
 }
--- src/Test/BookTests.java
+++ src/Test/BookTests.java
@@ -1,4 +1,78 @@
 package Test;
 
+import Main.Book;
+import Main.BookInterface;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 public class BookTests {
+    private BookInterface book;
+
+    @BeforeEach
+    public void setUp(){
+        book = new Book(
+                "123456",
+                "Atomic Habits",
+                "James Clear",
+                2018
+        );
+    }
+
+    @Test
+    public void testGetIsbn(){
+        assertEquals("123456",book.getIsbn());
+    }
+
+    @Test
+    public void testGetTitle(){
+        assertEquals("Atomic Habits",book.getTitle());
+    }
+
+    @Test
+    public void testGetAuthor(){
+        assertEquals("James Clear", book.getAuthor());
+    }
+
+    @Test
+    public void testGetYear(){
+        assertEquals(2018,book.getYear());
+    }
+
+    @Test
+    public void testBookBorrowSuccess() throws Exception {
+        try{
+            book.bookBorrow();
+        }catch(Exception e){
+            throw new Exception(e);
+        }
+        assertTrue(book.isBorrowed());
+    }
+
+    @Test
+    public void testBookBorrowFailure() throws Exception {
+        try{
+            book.bookBorrow();
+        }catch(Exception e){
+            throw new Exception(e);
+        }
+        assertThrows(Exception.class, ()-> book.bookBorrow());
+    }
+
+    @Test
+    public void testBookReturnSucess() throws Exception {
+        try{
+            book.bookBorrow();
+        }catch(Exception e){
+            throw new Exception(e);
+        }
+
+        book.bookReturn();
+        assertFalse(book.isBorrowed());
+    }
+
+    @Test
+    public void testBookReturnFailure(){
+        assertThrows(Exception.class, ()-> book.bookReturn());
+    }
 }
