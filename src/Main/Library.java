597deab2e240e4bf0740cab101f34fffdbc4dd0f
Shruti2208 <shindeshru2208@gmail.com>
2024-09-27 01:55:07 +0530

    Implemented libraryInterface and Library class

--- src/Main/Library.java
+++ src/Main/Library.java
@@ -1,4 +1,58 @@
 package Main;
 
-public class Library {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Library implements LibraryInterface{
+
+    private final Map<String, BookInterface> books;
+
+    public Library() {
+        this.books = new HashMap<>();
+    }
+
+    @Override
+    public void addBook(BookInterface book) throws Exception {
+        if(books.containsKey(book.getIsbn())){
+            throw new Exception("Book Already Exists.");
+        }
+        books.put(book.getIsbn(),book);
+    }
+
+    @Override
+    public void bookBorrow(String isbn) throws Exception {
+        BookInterface book = books.get(isbn);
+        if(book!=null){
+            try{
+                book.bookBorrow();
+            }catch(Exception e){
+                throw new Exception(e);
+            }
+        }else{
+            throw new Exception("Book not found!!");
+        }
+
+    }
+
+    @Override
+    public void bookReturn(String isbn) throws Exception {
+        BookInterface book = books.get(isbn);
+        if(book!=null){
+            if(!book.isBorrowed()){
+                throw new Exception("Book not borrowed");
+            }
+            book.bookReturn();
+        }else{
+            throw new Exception("Book not found!!");
+        }
+    }
+
+    @Override
+    public List<BookInterface> viewAvailabeBooks() {
+        return books.values().stream()
+                .filter(book -> !book.isBorrowed())
+                .collect(Collectors.toList());
+    }
 }
--- src/Main/LibraryInterface.java
+++ src/Main/LibraryInterface.java
@@ -1,4 +1,10 @@
 package Main;
 
+import java.util.List;
+
 public interface LibraryInterface {
+    void addBook(BookInterface book) throws Exception;
+    void bookBorrow(String isbn) throws Exception;
+    void bookReturn(String isbn) throws Exception;
+    List<BookInterface> viewAvailabeBooks();
 }
